41c9522360d450945978642b05bb0fab1e9411b3
TheKodeToad <TheKodeToad@proton.me>
2026-03-17 20:11:42 +0000

    Add get channel

--- internal/api/routes/channels.go
+++ internal/api/routes/channels.go
@@ -5,11 +5,34 @@ import (
 
 	"github.com/TheKodeToad/fline/internal/api"
 	"github.com/TheKodeToad/fline/internal/config"
+	"github.com/TheKodeToad/fline/internal/convert"
+	"github.com/TheKodeToad/fline/internal/discord"
+	"github.com/TheKodeToad/fline/internal/fluxer"
 	"github.com/go-chi/chi/v5"
 )
 
 func channelsRouter(conf *config.Config, client http.Client) chi.Router {
 	router := chi.NewRouter()
+
+	router.Method("GET", "/{channel_id}", api.ProxyHandler[any, fluxer.Channel]{
+		Conf: conf,
+		Client: client,
+		Path: "/channels/{channel_id}",
+		MapResponse: func(inChannel fluxer.Channel) (any, error) {
+			outChannel, ok := convert.ChannelToDiscord(inChannel)
+			if !ok {
+				return nil, api.Error{
+					APIError: discord.APIError{
+						Code: discord.APIErrorUnknownChannel,
+						Message: "Unknown Channel",
+					},
+					Status: http.StatusNotFound,
+				}
+			}
+
+			return outChannel, nil
+		},
+	})
 
 	router.Mount("/{channel_id}/messages", messagesRouter(conf, client))
 
